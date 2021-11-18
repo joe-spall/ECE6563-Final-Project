534874e04dd98241aa37631fd2898f0c9dc4e043
Joe Spall <jspall98@aol.com>
2021-11-18 15:23:28 -0500

    Adds loopable functionality with statistics

---
+++ experiment_runner.m
@@ -0,0 +1,31 @@
+clear;close all;clc
+init
+
+num_loops = 30;
+success_count = 0;
+total_iterations = 0;
+fprintf('Running %d tests \n', num_loops);
+for i = 1:num_loops
+    fprintf('Running #%d ',i);
+    [connected,num_iterations] = main('NumberOfRobots',2,     ...
+                                      'NumberOfLeaders',1,    ...
+                                      'MaxIterations',5000,   ...
+                                      'VisibilityAngle',2*pi, ...
+                                      'VisibilityDist', 0.5,  ...
+                                      'ShowFigure', false,    ...
+                                      'InitialConditions', [], ...
+                                      'RoboDebug', false); 
+    if connected
+        success_count = success_count+1;
+        total_iterations = total_iterations+num_iterations;
+        fprintf('Success! \n');
+    else
+    	fprintf('Fail! \n');
+    end
+
+end
+fprintf('Done \n');
+%% 
+fprintf('Success Percentage: %.2f%% \n',round(success_count/ num_loops*100,2));
+fprintf('Average Success Duration: %.2f \n',round(total_iterations/ success_count,2));
+
