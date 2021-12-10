42393553e0890e31cef1f676ef97058ccf00198c
Juan-Elizondo <jelizo4578@gmail.com>
2021-12-10 17:35:33 -0500

    updated follower decisions

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 % In this experiment edges between robots will be plotted as well as robot
 % labels and leader goal locations.
 
-% Sean Wilson and Joseph Spall
+% Sean Wilson, Joseph Spall, and Juan Elizondo
 % 07/2019
 
 function [connected,num_iterations] = main(varargin) 
@@ -49,13 +49,12 @@ function [connected,num_iterations] = main(varargin)
 
 
     % These are gains for our formation control algorithm
-    formation_control_gain = 5;
-    min_distance = 1.6*robot_diameter;
-    max_distance = 0.9*(visibility_dist-min_distance);
+    min_distance = 1.2*robot_diameter;
+    max_distance = 0.8*(visibility_dist-min_distance);
     line_angle = parser.Results.LineFormationAngle;
 
     % Leader
-    leader_waypoint_dist = 0.05;
+    leader_waypoint_dist = robot_diameter;
     leader_color = 'r';
 
     % Follower
@@ -79,9 +78,6 @@ function [connected,num_iterations] = main(varargin)
     % Initialize velocity vector
     dxi = zeros(2, N);
 
-    % Tracks previous theta for rotation tracking
-    old_theta = zeros(1,N);
-
     % States
     L_states = 1:N_L; %Leaders
     F_states = N_L+1:N; %Followers
@@ -93,9 +89,9 @@ function [connected,num_iterations] = main(varargin)
     % Single-integrator -> unicycle dynamics mapping
     si_to_uni_dyn = create_si_to_uni_dynamics('LinearVelocityGain', 0.8);
     % Single-integrator barrier certificates
-    uni_barrier_cert = create_uni_barrier_certificate_with_boundary();
+    uni_barrier_cert = create_uni_barrier_certificate_with_boundary('BoundaryPoints',r.boundaries);
     % Single-integrator position controller
-    leader_controller = create_si_position_controller('XVelocityGain', 0.8, 'YVelocityGain', 0.8, 'VelocityMagnitudeLimit', 0.9);
+    leader_controller = create_si_position_controller('XVelocityGain', 0.8, 'YVelocityGain', 0.8, 'VelocityMagnitudeLimit', 0.09);
 
     %% Plotting Setup
     if show_figure
@@ -118,7 +114,6 @@ function [connected,num_iterations] = main(varargin)
         visibility_plot = gobjects(1,N);
     
         % Marker, font, and line sizes
-        marker_size_robot = determine_robot_marker_size(r,visibility_dist);
         marker_size_goal = determine_marker_size(r, 0.2);
         font_size = determine_font_size(r, 0.05);
         line_width = 3;
@@ -187,6 +182,10 @@ function [connected,num_iterations] = main(varargin)
 
         %% Follower Controller
 
+        followed_leaders = zeros(N_L,2);
+        following_leaders = zeros(N,1);
+        following_leaders(L_states) = 1;
+        targets = zeros(N,1);
         for i = F_states
 
             %Zero velocity and get the topological neighbors of agent i
@@ -194,30 +193,53 @@ function [connected,num_iterations] = main(varargin)
 
             if any(A(i,:))
                 neighbors = find(A(i,:)==1);
-                target = neighbors(1);
+                if any(following_leaders(neighbors))
+                    [~,~,targets(i)] = find(following_leaders(neighbors),1);
+                    followed_leaders(targets(i),mod(i,2)+1) = 1;
+                    following_leaders(i) = 1;
+                else
+                    targets(i) = neighbors(1);
+                end
                 for j = neighbors
-                    %looking for i-1 robot
-                    %if not available, then i-2, and so on
-                    if (i - j) > 0
-                        if (i - j) < (i - target)
-                            target = j;
+                    if any(j==L_states)
+                        if (followed_leaders(j,mod(i,2)+1)==0)
+                            targets(i) = j;
+                            followed_leaders(j,mod(i,2)+1) = 1;
+                            following_leaders(i) = 1;
+                            break;
+                        end
+                    else
+                        if following_leaders(j)==1
+                            if ((i - j) > 0)
+                                if ((i - j) < (i - targets(i)))
+                                    targets(i) = j;
+                                    following_leaders(i) = 1;
+                                end
+                            elseif ((i - targets(i)) > 0) && ((i - j) < 0)
+                                break;
+                            else
+                                if ((j - i) < (targets(i) - i))
+                                    targets(i) = j;
+                                    following_leaders(i) = 1;
+                                end
+                            end
                         end
                     end
                 end
                 x_p = [min_distance*cos(line_angle);min_distance*sin(line_angle)];
-                x_p_g = robot_to_global(x(:,target),x_p);
+                x_p_g = robot_to_global(x(:,targets(i)),x_p);
                 radius = norm([x(1,i), x(2,i)] - [x_p_g(1), x_p_g(2)]);
-                if radius < max_distance
-                    %wij = (1-min_distance/radius)/((max_distance-radius)^3);
+                if radius < 0.95*max_distance
                     wij = 1/((max_distance-radius)^3);
-                    dxi(:, i) = dxi(:, i) + wij*(x_p_g - x(1:2, i));
+                    dxi(:, i) = dxi(:, i) + 10*wij*(x_p_g - x(1:2, i));
                 else
-                    dxi(:, i) = dxi(:, i) + (x_p_g - x(1:2, i));
+                    dxi(:, i) = dxi(:, i) + 10*(x_p_g - x(1:2, i));
                 end
             else
                 dxi(:, i) = [0;0];
             end
         end
+
 
         %% Leader Controller
         
@@ -389,12 +411,12 @@ function x_out = robot_to_global(x_robot, x_in)
     x_out = x_new(1:2);
 end
 
-function [x_out, y_out] = norm_coord(x, y, axes, xlims, ylims)
-    x_out = ((x-xlims(1))/(xlims(2) - xlims(1)))*axes(3);
-    y_out = ((y-ylims(1))/(ylims(2) - ylims(1)))*axes(4);
-    x_out = axes(1) + x_out;
-    y_out = axes(2) + y_out;
-end       
+% function [x_out, y_out] = norm_coord(x, y, axes, xlims, ylims)
+%     x_out = ((x-xlims(1))/(xlims(2) - xlims(1)))*axes(3);
+%     y_out = ((y-ylims(1))/(ylims(2) - ylims(1)))*axes(4);
+%     x_out = axes(1) + x_out;
+%     y_out = axes(2) + y_out;
+% end       
 
 % Marker Size Helper Function to scale size with figure window
 % Input: robotarium instance, desired size of the marker in meters
